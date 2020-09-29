872716c94622c4b64c060efd87c05849b54ac793
pratik1312 <patilpratik194@gmail.com>
2020-09-29 16:34:23 +0530

    Added:file computes the first arithmetic expression

--- ArithmeticCompute.sh
+++ ArithmeticCompute.sh
@@ -1,4 +1,6 @@
-echo "Welcome to arithmetic Computation and Sorting Program"
+##!/bin/bash -x
 read -p "Enter the first number: " a
-read -p "Enter the second number: "b
-read -p "Enter the third number: "c
+read -p "Enter the second number: " b
+read -p "Enter the third number: " c
+op1=$(($a+$b*$c))
+echo $op1
