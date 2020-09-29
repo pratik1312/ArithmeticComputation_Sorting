e9235dbda3109396c87e17f01e7f32422694a680
pratik1312 <patilpratik194@gmail.com>
2020-09-29 18:57:11 +0530

    Added:Results expression stored in dictionary

--- ArithmeticCompute.sh
+++ ArithmeticCompute.sh
@@ -1,12 +1,21 @@
 ##!/bin/bash -x
+declare -A Operations
 read -p "Enter the first number: " a
 read -p "Enter the second number: " b
 read -p "Enter the third number: " c
 op1=$(($a+$b*$c))
-echo $op1
+echo "Result of first expression : "$op1
 op2=$(($a*$b+$c))
-echo $op2
+echo "Result of second expression : "$op2
 op3=$(($c+$a/$b))
-echo $op3
+echo "Result of third expression : "$op3
 op4=$(($a%$b+$c))
-echo $op4
+echo "Result of fourth expression : "$op4
+
+Operations[op1]=$op1
+Operations[op2]=$op2
+Operations[op3]=$op3
+Operations[op4]=$op4
+
+echo "The results in dictionary : "${Operations[@]}
+
